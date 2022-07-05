49400f423f10c67ffe0a57066f4f7302723bb7cd
Kholifatulardhi08 <ardhi.kholifatul8@gmail.com>
2022-07-05 22:14:16 +0700

    login page
    Signed-off-by: Kholifatulardhi08 <ardhi.kholifatul8@gmail.com>

---
+++ database/migrations/2022_07_05_114800_create_bookings_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBookingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->foreignId('instances_id')->consrained();
+            $table->foreignId('rooms_id')->consrained();
+            $table->foreignId('pantries_id')->consrained();
+            $table->date('start_date')->nullalble();
+            $table->date('end_date')->nullalble();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bookings');
+    }
+}
