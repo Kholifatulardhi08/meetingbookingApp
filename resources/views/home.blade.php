38a3d6b38c0acfc8e1e062547170eeb36c14d2a7
Kholifatulardhi08 <ardhi.kholifatul8@gmail.com>
2022-07-11 10:25:57 +0700

    edit tampilan
    Signed-off-by: Kholifatulardhi08 <ardhi.kholifatul8@gmail.com>

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,19 @@
 
 @section('content')
     <!-- Page Heading -->
-    <div class="d-sm-flex align-items-center justify-content-between mb-4">
-        <h2 class="h3 mb-0 text-gray-800">Dashboard</h2>
+    <div class="container">
+        <div class="row">
+            <div class="d-sm-flex align-items-center justify-content-between mb-4">
+                <h2 class="h3 mb-0 text-gray-800">Dashboard</h2>
+            </div>
+            <div class="col-md-10 offset-md-1">
+                <div class="card">
+                    <div class="card-header">Dashboard</div>
+                    <div class="card-body">
+                        You are logged in!
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
